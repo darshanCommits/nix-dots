6b251e2302dae844ba6a2106c627a664863f2b63
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2024-12-27 19:19:14 +0530

    done

--- system/de.nix
+++ system/de.nix
@@ -7,7 +7,7 @@
 }: {
   programs.uwsm = {
     enable = true;
-    waylandCompositorshyprland = {
+    waylandCompositors.hyprland = {
       prettyName = "Hyprland";
       comment = "Hyprland compositor managed by UWSM";
       binPath = "/run/current-system/sw/bin/Hyprland";
